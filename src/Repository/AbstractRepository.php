90d21d4a8997889b127720beafa2dbcdf05c66fc
selkniez <sam.elkniez@gmail.com>
2026-05-30 00:48:34 +0200

    feat: add initial project configuration and repository structure

---
+++ src/Repository/AbstractRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+abstract class AbstractRepository extends ServiceEntityRepository
+{
+    public function save(object $entity, bool $flush = true): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(object $entity, bool $flush = true): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
